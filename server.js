f44970cd9f9d56ff3d0c4e297875a731388d62af
Sonick Mumba <kingellie.mumba@gmail.com>
2026-01-27 16:14:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,24 +6,16 @@ const db = require('./config/db');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
-// Sample route to test database connection
-app.get('/test-db', async (req, res) => {
-  try {
-    const client = await db.connect();
-    const result = await client.query('SELECT NOW()');
-    client.release();
-    res.status(200).json({ message: 'Database connected', time: result.rows[0] });
-  } catch (err) {
-    res.status(500).json({ error: 'Database connection failed', details: err.message });
-  }
-});
 
-// Start the server
+
+/* ======================
+   START SERVER
+====================== */
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`API Server is running at http://localhost:${PORT}.`);
 }); 
