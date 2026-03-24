76fb396f27c86fc106127c7be957dcaac71a6103
Sonick Mumba <kingellie.mumba@gmail.com>
2026-03-25 01:06:14 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const poolConfig = process.env.DATABASE_URL
   ? {
       connectionString: process.env.DATABASE_URL,
       ssl:
-        process.env.NODE_ENV === 'production'
+        process.env.DATABASE_SSL === 'true'
           ? { rejectUnauthorized: false }
           : false,
     }
